46f7118ceb7f37a9fdba62aefa16c2c5649f3aaf
Andrey Yurchenkov <painhardcore@gmail.com>
2021-10-29 04:33:16 +0300

    easy
    Signed-off-by: Andrey Yurchenkov <painhardcore@gmail.com>

--- build.zig
+++ build.zig
@@ -340,7 +340,7 @@ const exercises = [_]Exercise{
     .{
         .main_file = "066_comptime.zig",
         .output = "Immutable: 12345, 987.654; Mutable: 54321, 456.789; Types: comptime_int, comptime_float, u32, f32",
-        .hint = "It may help to read this one out loud to your favorite stuffed animal until it sinks in completely."
+        .hint = "It may help to read this one out loud to your favorite stuffed animal until it sinks in completely.",
     },
     .{
         .main_file = "067_comptime2.zig",
